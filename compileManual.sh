e5fb93f03cbf129a37f40cd66dbd2c26394476d8
Simon <Simon.Hobbs@electrooptical.net>
2017-05-01 21:22:12 -0400

    initial commit

---
+++ compileManual.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pdflatex triggerManual
+sleep 2
+biber triggerManual
+sleep 2
+makeglossaries triggerManual
+sleep 2
+pdflatex triggerManual
+sleep 5
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=smallTriggerManual.pdf triggerManual.pdf
